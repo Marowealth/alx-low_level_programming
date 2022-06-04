e5885478f63c19e98c6e3c3f64e637f6950784a4
Marowealth <marolovey9@gmail.com>
2022-06-04 04:12:44 -0700

    print 3comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,18 +18,18 @@ int main(void)
 		k = i / 10; /* doubles digit */
 
 		if (k < j)
-			{
-				putchar(k + '0');
-				putchar(j + '0');
+		{
+			putchar(k + '0');
+			putchar(j + '0');
 
-				if (i < 89)
-				{
-					putchar(44);
-					putchar(32);
-					
-				}
+			if (i < 89)
+			{
+			putchar(44);
+			putchar(32);
+
 			}
-			i++;
+		}
+		i++;
 	}
 	putchar('\n');
 	return (0);
