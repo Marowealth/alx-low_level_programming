cd8923bc154943ebc5e39b490f3b414702d776b2
Marowealth <marolovey9@gmail.com>
2022-06-03 17:06:12 -0700

    print

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,9 +2,8 @@
 
 /*
  * main - printing alphabet.
- *
+ * Description: Use 'putchar' to print lowercase and then alphabet.
  * Return: Always 0 (Success)
- *
 */
 
 int main(void)
@@ -14,7 +13,6 @@ int main(void)
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		putchar(letter);
-
 	}
 
 	for (letter = 'A'; letter <= 'Z'; letter++)
