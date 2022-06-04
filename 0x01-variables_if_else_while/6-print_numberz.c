3f09d10c2d2a8331c3810d4a138037745423a7a2
Marowealth <marolovey9@gmail.com>
2022-06-03 17:43:45 -0700

    6 print

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,12 @@
  *
  *Return: Always 0 (Success)
 */
+
 int main(void)
 {
 	int number;
 
-	for (number = 0; number < 10; number++)
+	for (number = '0'; number <= '9'; number++)
 	{
 		putchar(number);
 	}
