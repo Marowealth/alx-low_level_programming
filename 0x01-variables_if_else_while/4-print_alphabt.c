5319a1b292267005bfc2fafb9b9e28192f6f5a03
Marowealth <marolovey9@gmail.com>
2022-06-03 17:19:01 -0700

    4 print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print letters without q and e
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+
+		for (letter != 'e'; && letter != 'q')
+		{
+			putchar(letter);
+		}
+	}
+	putchar('\n');
+	return (0)
+}
