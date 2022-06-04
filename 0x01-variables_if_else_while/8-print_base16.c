20a6d758a0710af84db0b338f527c953e7735ac0
Marowealth <marolovey9@gmail.com>
2022-06-03 23:05:15 -0700

    alphabet base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers in base 16.
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	for (i = 'a'; i <= 'f'; i++)
+	{
+		putchar(i);
+	{
+
+	putchar('\n');
+	return (0);
+}
