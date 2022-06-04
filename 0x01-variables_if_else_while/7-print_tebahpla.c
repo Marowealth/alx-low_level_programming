51f086a60e7397fc24ff2abdc1f8897debf27aac
Marowealth <marolovey9@gmail.com>
2022-06-03 22:42:47 -0700

    alpha rev

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print tebahpla.
+ *Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char alpha_rev
+
+	for (alph_rev = 'z'; alph_rev <= 'a'; alph_rev++)
+	{
+		putchar(alph_rev);
+	}
+
+	putchar('\n');
+	return (0);
+}
