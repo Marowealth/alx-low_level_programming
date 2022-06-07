470c3e98154f4ba8887e9cff3bd4c19c3d2f2b29
Marowealth <marolovey9@gmail.com>
2022-06-07 10:23:05 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,8 @@
 
 /**
  * _isalpha - checks for alphabetic character
- * @c: c is an ascii character
- * Return: 1 (if letter)
+ * @c: type int character
+ * Return: 1 if letter, lowercase or uppercase, and 0 otherwise
 */
 int _isalpha(int c)
 {
