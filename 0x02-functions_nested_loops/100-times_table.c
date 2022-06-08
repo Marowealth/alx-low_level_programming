938fdcfc8344a776eff5dbe045e177a3e58710bc
Marowealth <marolovey9@gmail.com>
2022-06-08 03:56:01 -0700

    100 times

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,99 @@
 #include "main.h"
 
 /**
- * print_times_table - Print the 'n' times table, starting with 0.
- * Description: if 'n' is greater than 15 or less than 0, print nothing
- * @n: int type number
-*/
+
+ * print_times_table - function that prints the n times table, starting with 0.
+
+ * @n: input of table n
+
+ * Return: nothing.
+
+ */
+
+
+
 void print_times_table(int n)
+
 {
-	int x, y, z;
 
-	if (n > 15 || n < 0)
+	int row, col;
+
+	int mult = 0;
+
+
+
+	if (n <= 15 && n >= 0)
+
 	{
-		for (y = 0; y <= n; y++)
-		{
-			z = (x * y);
 
-                        if (y != n)                                     
-                        {                                               
-                                _putchar(';');                          
-				_putchar(' ');
-			}
-			if (z >=100 && y != 0)
+		for (row = 0; row <= n; row++)
+
+		{
+
+			for (col = 0; col <= n; col++)
+
 			{
-				_putchar((z / 100) + '0');
-				_putchar((z / 10) % 10) + '0');
-				_putchar((z % 10 + '0');
+
+				mult = (row * col);
+
+				if (mult < 10)
+
+				{
+
+					if (col != 0)
+
+					{
+
+						_putchar(' ');
+
+						_putchar(' ');
+
+						_putchar(' ');
+
+					}
+
+					_putchar(mult + '0');
+
+				}
+
+				else if (mult < 100)
+
+				{
+
+					_putchar(' ');
+
+					_putchar(' ');
+
+					_putchar((mult / 10) + '0');
+
+					_putchar((mult % 10) + '0');
+
+				}
+
+				else
+
+				{
+
+					_putchar(' ');
+
+					_putchar((mult / 100) + '0');
+
+					_putchar(((mult % 100) / 10) + '0');
+
+					_putchar((mult % 10) + '0');
+
+				}
+
+				if (col != n)
+
+					_putchar(',');
+
 			}
-			else if (z >= 10 && z < 100)
-			{
-				_putchar(' ');
-				_putchar((z / 10) + '0');
-				_putchar((z % 10) + '0');
-			}
-			else if (z < 10 && y != 0)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar((z % 10) + '0');
-			}
-			else
-				_putchar((z % 10) + '0');
+
+			_putchar('\n');
 
 		}
-		_putchar('\n');
-		x++;
+
 	}
+
 }
