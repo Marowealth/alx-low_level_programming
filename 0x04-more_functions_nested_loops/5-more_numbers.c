cbdd4469d2d777cd8a4a4ca6ef0c30b507676b20
Marowealth <marolovey9@gmail.com>
2022-06-09 12:22:09 -0700

    num

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,15 +8,15 @@ void more_numbers(void)
 {
 	int replay, count;
 
-	for (replay = 0; replay <= 9; replay++)
+	for (replay = 0; replay < 10; replay++)
 	{
 		for (count = 0; count <= 14; count++)
 		{
-			if (count >= 10 && count <= 14)
-			{
+			if (count > 9)
+			
 				_putchar((count / 10) + '0');
-			}
-				_putchar((count % 10) + '0');
+			_putchar((count % 10) + '0');
+			
 		}
 	_putchar('\n');
 }
