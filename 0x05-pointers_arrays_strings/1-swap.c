a18e5cbebd9c065066b9dd6b4bfeb904182ba5f6
Marowealth <marolovey9@gmail.com>
2022-06-13 08:52:41 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * swap_int - funtioncs that swaps 2 int 
- * Return: Void 
+ * swap_int - funtioncs that swaps 2 int
+ * Return: Void
 */
 void swap_int(int *a, int *b)
 {
 	int changed;
-	
+
 	changed = *a;
 	*a = *b;
 	*b = changed;
