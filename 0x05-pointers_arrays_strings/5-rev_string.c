3705a95b0fa1914944e952a56f72304809ee45dc
Marowealth <marolovey9@gmail.com>
2022-06-13 11:11:02 -0700

    rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * rev_string - function that reverse a string
+ * @s: string type
+ * Return: void
+*/
+void rev_string(char *s)
+{
+	int i, c, l;
+
+	for (i = 0, s[i] != '\0'; i++
+		;
+
+	l = i;
+	for (i--, c = 0; c < l / 2; i--, c++)
+	{
+		h = s[c];
+		s[c] = s[i];
+		s[i] = h;
+	}
+}
