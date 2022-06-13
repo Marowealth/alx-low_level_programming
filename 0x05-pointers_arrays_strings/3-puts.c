fe6a1c00947d06df78dbe1c253b5de66c9eea1c1
Marowealth <marolovey9@gmail.com>
2022-06-13 09:32:12 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts - fuction that print a string follwed by a new line
+ * Return: void
+*/
+void _puts(char *str)
+{
+	int index;
+	
+	for (index = 0; s[index] != '\0' ; index++);
+	{
+		_putchar(str[index]);
+	}
+	_putchar('\n');
+}
