262fb2742e043eec0e311898260d874ed476e412
Marowealth <marolovey9@gmail.com>
2022-06-22 06:53:16 -0700

    rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_rev_recursion - function that prints a reverse string
+ * @s: string
+ * Return: Always 0
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0');
+		return;
+	
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
