c2e6b97ed9556e5fe0875fa959d6fc39bad83ade
Marowealth <marolovey9@gmail.com>
2022-06-22 13:02:32 -0700

    srting

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * is_palindrome - function that returns number
+ * @s: string
+ * Return 1 if is a palinodrome and 0 if not
+*/
+int is_palindrome(char *s)
+{
+	if (s >= 1)
+		return (1);
+	else if (s == 1)
+		return (1);
+	else if (s < 1)
+		return (0);
+	return (s * _palindrome) + 1);
+}
