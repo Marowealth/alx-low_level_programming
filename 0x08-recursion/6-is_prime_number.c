144887094b02a60795c7cfdc6c8ca6bbec61b89b
Marowealth <marolovey9@gmail.com>
2022-06-22 12:49:16 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,11 +7,13 @@
 */
 int is_prime_number(int n)
 {
+	int n = prime;
+
 	if (n <= 1)
 	       return (0);
 	else if (n == 2)
 		return (1);
-	else if (n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
+	else if ((n % 2 == 0) || (n % 3 == 0) || (n % 5 == 0))
 		return (0);
 	else if (n % 7 == 0 || n % 9 == 0)
 		return (0);
