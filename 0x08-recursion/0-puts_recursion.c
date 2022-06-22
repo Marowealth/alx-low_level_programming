8483f34e2b7db1aeb1a19804a3d65faebbbcc913
Marowealth <marolovey9@gmail.com>
2022-06-22 06:37:36 -0700

    puts

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,9 +2,15 @@
 
 /**
  * _puts_recursion - function that prints a string
+ * @s: string
  * Return: void
 */
 void _puts_recursion(char *s)
 {
-	_putchar("puts with recursion\n");
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+
 }
