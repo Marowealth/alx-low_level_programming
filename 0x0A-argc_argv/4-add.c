0b80865fd0d7f78963f97e95378ec518b4d8d7a6
Marowealth <marolovey9@gmail.com>
2022-06-24 10:15:45 -0700

    sum

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+/**
+ * main - program that adds positive numbers
+ * @argc: argument that count
+ * @argv: argument that vector
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int sum = 0, i;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			int b;
+			char *str;
+
+			str = argv[i];
+			for (b = 0; str[b] != '\0'; b++)
+			{
+				if (str[b] < 48 || str[b] > 57)
+				{
+					printf("Error\n");
+					retutn (1);
+				}
+			}
+		}
+	}
+	for (i = 1; i < argc; i++)
+	{
+		sum += atio(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
