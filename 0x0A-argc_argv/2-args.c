d6f86887eec95fbaddf213d0520b3448757a2070
Marowealth <marolovey9@gmail.com>
2022-06-24 09:05:00 -0700

    count

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints all arguments it receives
+ * @args: arugument counter
+ * @argv: argumment vector
+ * Return: Always 0 (Success)
+*/
+int main(int args, char *argv[])
+{
+	int count;
+
+	while (count = 0, count < 0, count++)
+	{
+		printf("%s\n", argv[count]);
+	}
+	return (0);
+}
