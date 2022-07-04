2759828517f7d056c40471dd057a8c8a1a79cab3
Marowealth <marolovey9@gmail.com>
2022-07-04 13:03:13 -0700

    main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,8 @@
 #include <stdlib.h>
 
 /**
- * main - program that prints the name of the file it was compiled from, followed by a new line.
+ * main - program that prints the name of the file it was compiled from,
+ * followed by a new line.
  * Return: Always 0 (Success)
 */
 
