1815aaed36c2a09226ce33e5d2a427ac3259565c
Marowealth <marolovey9@gmail.com>
2022-07-14 15:06:16 -0700

    first

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,21 +5,12 @@
  * so that it is executed before main()
  */
 
-	void startupfun(void) __attribute__ ((constructor));
-
-/**
-* Apply the destructor attribute to cleanupfun()
-*so that it is executed after main()
-*/
-
-	void cleanupfun(void)__attribute__ ((destructor));
-
+	void startupfun(void) __attribute__ ((destructor));
 /**
  * startupfun - This functions print a text
- *
  */
-void startupfun(void)
-{
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
-}
+	void startupfun(void)
+	{
+		printf("You're beat! and yet, you must allow,");
+		printf("\nI bore my house upon my back!\n");
+	}
