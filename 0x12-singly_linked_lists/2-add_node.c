d35301596caab6b2367365e7ce1886cab520ff69
Marowealth <marolovey9@gmail.com>
2022-07-14 14:02:40 -0700

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,27 +26,13 @@ int len(const char *str)
  */
 list_t *add_node(list_t **head, const char *str)
 {
-		list_t *new_node;
-			int c;
+	list_t *new_node = (list_t *) malloc(sizeof(list_t));
 
-	for (c = 0; dup_str[c] != '\0'; c++)
-		;
-	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
-	{
-		free(new_node);
 		return (NULL);
-	}
 	new_node->str = strdup(str);
-	if (new_node->str == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->len = c;
-	new_node->next = *head;
-
-	*head = new_node;
+	new_node->len =  len(str);
+	new_node->next = (*head);
+	(*head) = new_node;
 	return (*head);
-
 }
