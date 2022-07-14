c713bb9bc6adb62e7282405aee51c0819fd385a5
Marowealth <marolovey9@gmail.com>
2022-07-14 13:59:33 -0700

    add

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,22 @@
 #include <stdio.h>
 
 /**
+ * len - Calculates the length of a string.
+ * @str: Constant string
+ * Return: The length of the string
+ */
+int len(const char *str)
+{
+		int count;
+
+		if (str == NULL)
+		return (0);
+	for (count = 0; str[count] != '\0'; count++)
+		;
+	return (count);
+}
+
+/**
  * add_node - add a new node at the beginning of `list_t` list
  * @head: double pointer to head
  * @str: string to be saved in new node, must be duplicated
